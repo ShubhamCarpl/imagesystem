e032adfdadded68259b3a94e3772b57868a907ad
ShubhamCarpl <shubham.garg@carpl.ai>
2022-07-11 14:36:41 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import Login from './login';
+import Login from './components/login';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./home"
-
+import Home from "./components/home"
+import Header from './components/header';
+import Footer from './components/footer';
 function App() {
   return (
   <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path='/' element= {<Login/>}/>
     <Route path='/home' element={<Home/>}/>
     </Routes>
-    </BrowserRouter>
+    <Footer/>
+  </BrowserRouter>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div>developed by tech interns at Carpl.ai</div>
+  )
+}
+
+export default footer
